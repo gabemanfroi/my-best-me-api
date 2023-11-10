68123630dc943fdb3dc2e91b3e93b056bc409b40
Gabriel Manfroi <gabriel.manfroi@edreamsodigeo.com>
2023-11-10 00:55:01 +0000

    :hammer: adds getters and setter annotation

--- src/main/java/com/example/mybestme/domain/models/LearningResource.java
+++ src/main/java/com/example/mybestme/domain/models/LearningResource.java
@@ -7,13 +7,17 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 
 @Entity
 @Table(name = "learning_resource")
 @Data
 @NoArgsConstructor
+@Getter
+@Setter
 public class LearningResource {
 
     @Id
